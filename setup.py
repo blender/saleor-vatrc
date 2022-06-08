7360b0b5af8c26c6980c3d0e9332e786b81050b0
Anna Sirota <anna@blender.org>
2022-06-08 14:28:00 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="saleor_vatrc",
-    version="0.1",
+    version="0.2",
     description="Implements VAT reverse charge in Saleor",
     author="Anna Sirota",
     author_email="anna@blender.org",
